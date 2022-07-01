5b384b5dfb0dd512c4192e375bbc31a1054070e9
aramalipoor <aram.alipoor@gmail.com>
2022-07-01 10:38:16 +0200

    feat: deploy factory on all chains

--- contracts/tokens/presets/ERC20BasicToken.sol
+++ contracts/tokens/presets/ERC20BasicToken.sol
@@ -2,14 +2,24 @@
 
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract ERC20BasicToken is ERC20, ERC20Burnable, Pausable, AccessControl {
+contract ERC20BasicToken is
+    Initializable,
+    ERC20,
+    ERC20Burnable,
+    Pausable,
+    AccessControl
+{
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    string private _name;
+    string private _symbol;
 
     struct Config {
         string name;
@@ -17,9 +27,27 @@ contract ERC20BasicToken is ERC20, ERC20Burnable, Pausable, AccessControl {
     }
 
     constructor(Config memory config) ERC20(config.name, config.symbol) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
+        initialize(config, msg.sender);
+    }
+
+    function initialize(Config memory config, address deployer)
+        public
+        initializer
+    {
+        _name = config.name;
+        _symbol = config.symbol;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, deployer);
+        _grantRole(PAUSER_ROLE, deployer);
+        _grantRole(MINTER_ROLE, deployer);
+    }
+
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
--- contracts/tokens/presets/ERC20LockableToken.sol
+++ contracts/tokens/presets/ERC20LockableToken.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
@@ -12,6 +13,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "../extensions/ERC20RoleBasedLockingExtension.sol";
 
 contract ERC20LockableToken is
+    Initializable,
     ERC165Storage,
     AccessControl,
     ERC20,
@@ -22,15 +24,36 @@ contract ERC20LockableToken is
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    string private _name;
+    string private _symbol;
+
     struct Config {
         string name;
         string symbol;
     }
 
     constructor(Config memory config) ERC20(config.name, config.symbol) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
+        initialize(config, msg.sender);
+    }
+
+    function initialize(Config memory config, address deployer)
+        public
+        initializer
+    {
+        _name = config.name;
+        _symbol = config.symbol;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, deployer);
+        _grantRole(PAUSER_ROLE, deployer);
+        _grantRole(MINTER_ROLE, deployer);
+    }
+
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
     }
 
     /* ADMIN */
