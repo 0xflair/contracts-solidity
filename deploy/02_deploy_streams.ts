3842dc161ec0c52dde684ec9fb59270707032fc8
aramalipoor <aram.alipoor@gmail.com>
2022-08-05 10:36:38 +0200

    fix: deploy custodial staking preset

--- deploy/02_deploy_streams.ts
+++ deploy/02_deploy_streams.ts
@@ -60,6 +60,26 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       },
     ]
   );
+  await deployPermanentContract(
+    hre.deployments,
+    accounts[0],
+    accounts[0],
+    "ERC721CustodialStakingEmissionStream",
+    [
+      {
+        ticketToken: "0x0000000000000000000000000000000000000000",
+        lockedUntilTimestamp: 0,
+        minStakingDuration: 0,
+        maxStakingTotalDurations: 0,
+        emissionRate: 0,
+        emissionTimeUnit: 0,
+        emissionStart: 0,
+        emissionEnd: 0,
+        totalTickets: 0,
+        claimLockedUntil: 0,
+      },
+    ]
+  );
 };
 
 export default func;
