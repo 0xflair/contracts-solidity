d15b689ce73d9d04e8df902a00ec627c74d6a77d
aramalipoor <aram.alipoor@gmail.com>
2022-09-22 16:42:53 +0200

    chore: deploy all to goerli network

--- test/contracts/ERC1155MintByFacet.spec.ts
+++ test/contracts/ERC1155MintByFacet.spec.ts
@@ -1,97 +1,65 @@
-import hre from "hardhat";
-import { expect } from "chai";
+import { expect } from 'chai';
+import hre from 'hardhat';
 
-import { deployDiamond } from "../utils/diamond";
-import { ERC1155Mintable, ERC1155MintByFacet, Multicall } from "../../typechain";
-import { setupTest } from "../setup";
+import { ERC1155Mintable, Multicall } from '../../typechain';
+import { setupTest } from '../setup';
+import { deployDiamond } from '../utils/diamond';
 
 const deployERC1155 = async () => {
   return await deployDiamond({
-    facets: ["ERC1155"],
+    facets: ['ERC1155'],
     initializations: [
       {
-        facet: "ERC1155SupplyOwnable",
-        function: "setMaxSupply",
+        facet: 'ERC1155SupplyOwnable',
+        function: 'setMaxSupply',
         args: [33, 1000],
       },
     ],
   });
 };
 
-describe("ERC1155Mintable", function () {
-  it("should not be able to mint when external account calls", async function () {
+describe('ERC1155Mintable', function () {
+  it('should not be able to mint when external account calls', async function () {
     const { userA } = await setupTest();
 
     const diamond = await deployERC1155();
 
-    const mintableFacet = await hre.ethers.getContractAt<ERC1155Mintable>(
-      "ERC1155Mintable",
-      diamond.address
-    );
+    const mintableFacet = await hre.ethers.getContractAt<ERC1155Mintable>('ERC1155Mintable', diamond.address);
 
-    await expect(
-      mintableFacet
-        .connect(userA.signer)
-        .mintByFacet(userA.signer.address, 33, 1, "0x")
-    ).to.be.revertedWith("SenderIsNotSelf()");
+    await expect(mintableFacet.connect(userA.signer).mintByFacet(userA.signer.address, 33, 1, '0x')).to.be.revertedWith(
+      'SenderIsNotSelf()',
+    );
   });
 
-  it("should not be able to mint when calling via multicall", async function () {
+  it('should not be able to mint when calling via multicall', async function () {
     const { userA } = await setupTest();
 
     const diamond = await deployERC1155();
 
-    const multiCallContract = await hre.ethers.getContractAt<Multicall>(
-      "Multicall",
-      diamond.address
-    );
+    const multiCallContract = await hre.ethers.getContractAt<Multicall>('Multicall', diamond.address);
 
-    const mintableFacet = await hre.ethers.getContractAt<ERC1155Mintable>(
-      "ERC1155Mintable",
-      diamond.address
-    );
+    const mintableFacet = await hre.ethers.getContractAt<ERC1155Mintable>('ERC1155Mintable', diamond.address);
 
-    const callData = mintableFacet.interface.encodeFunctionData("mintByFacet", [
-      userA.signer.address,
-      33,
-      1,
-      "0x",
-    ]);
+    const callData = mintableFacet.interface.encodeFunctionData('mintByFacet', [userA.signer.address, 33, 1, '0x']);
 
-    await expect(
-      multiCallContract.connect(userA.signer).multicall([callData])
-    ).to.be.revertedWith("SenderIsNotSelf()");
+    await expect(multiCallContract.connect(userA.signer).multicall([callData])).to.be.revertedWith('SenderIsNotSelf()');
   });
 
-  it("should not be able to mint when calling via nested multicall", async function () {
+  it('should not be able to mint when calling via nested multicall', async function () {
     const { userA } = await setupTest();
 
     const diamond = await deployERC1155();
 
-    const multiCallContract = await hre.ethers.getContractAt<Multicall>(
-      "Multicall",
-      diamond.address
+    const multiCallContract = await hre.ethers.getContractAt<Multicall>('Multicall', diamond.address);
+
+    const mintableFacet = await hre.ethers.getContractAt<ERC1155Mintable>('ERC1155Mintable', diamond.address);
+
+    const callData = mintableFacet.interface.encodeFunctionData('mintByFacet', [userA.signer.address, 33, 1, '0x']);
+
+    const callDataNested = multiCallContract.interface.encodeFunctionData('multicall', [[callData]]);
+
+    await expect(multiCallContract.connect(userA.signer).multicall([callDataNested])).to.be.revertedWith(
+      'SenderIsNotSelf()',
     );
-
-    const mintableFacet = await hre.ethers.getContractAt<ERC1155Mintable>(
-      "ERC1155Mintable",
-      diamond.address
-    );
-
-    const callData = mintableFacet.interface.encodeFunctionData("mintByFacet", [
-      userA.signer.address,
-      33,
-      1,
-      "0x",
-    ]);
-
-    const callDataNested = multiCallContract.interface.encodeFunctionData(
-      "multicall",
-      [[callData]]
-    );
-
-    await expect(
-      multiCallContract.connect(userA.signer).multicall([callDataNested])
-    ).to.be.revertedWith("SenderIsNotSelf()");
   });
 });
