e60edb590b03b56d7f253453e8bc342652bb14a7
aramalipoor <aram.alipoor@gmail.com>
2022-10-23 14:13:26 +0200

    fix: add view method to return current trusted forwarder

--- src/metatx/ERC2771Context.sol
+++ src/metatx/ERC2771Context.sol
@@ -9,6 +9,10 @@ import "./IERC2771Context.sol";
 contract ERC2771Context is IERC2771Context, ERC2771ContextInternal {
     using ERC2771ContextStorage for ERC2771ContextStorage.Layout;
 
+    function trustedForwarder() external view override returns (address) {
+        return ERC2771ContextStorage.layout().trustedForwarder;
+    }
+
     function isTrustedForwarder(address forwarder) public view virtual returns (bool) {
         return _isTrustedForwarder(forwarder);
     }
--- src/metatx/IERC2771Context.sol
+++ src/metatx/IERC2771Context.sol
@@ -3,5 +3,7 @@
 pragma solidity ^0.8.15;
 
 interface IERC2771Context {
+    function trustedForwarder() external view returns (address);
+
     function isTrustedForwarder(address forwarder) external view returns (bool);
 }
