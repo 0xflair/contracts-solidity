95c4fab8bcd4c42d05e2146168bfb0a0412fe2da
aramalipoor <aram.alipoor@gmail.com>
2022-11-28 21:49:21 +0100

    chore: deploy on all chains

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -5,3 +5,4 @@ cat package.json | \
     grep -oE '"([^"]+)":' | cut -d\" -f2 | \
     awk '{ print "npm run "$0; }' | \
     bash -c 'while read -r line; do echo "$line"; $line || true; done'
+    # || true
