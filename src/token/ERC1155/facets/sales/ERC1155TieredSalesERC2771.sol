fd1be9eff960190e64d159f44546fbcf175556a1
aramalipoor <aram.alipoor@gmail.com>
2022-12-01 19:00:13 +0100

    chore: add proper manifest for erc1155 sales facet

--- src/token/ERC1155/facets/sales/ERC1155TieredSalesERC2771.sol
+++ src/token/ERC1155/facets/sales/ERC1155TieredSalesERC2771.sol
@@ -6,7 +6,13 @@ import "../../../../metatx/ERC2771ContextInternal.sol";
 import "./ERC1155TieredSales.sol";
 
 /**
- * @dev Tiered Sales facet for ERC1155 with meta-transactions support via ERC2771
+ * @title ERC1155 - Tiered Sales - with meta-transactions
+ * @notice Sales mechanism for ERC1155 NFTs with ERC2771 meta-transactions support (e.g. credit card minting).
+ *
+ * @custom:type eip-2535-facet
+ * @custom:category NFTs
+ * @custom:required-dependencies IERC1155MintableExtension
+ * @custom:provides-interfaces ITieredSales
  */
 contract ERC1155TieredSalesERC2771 is ERC1155TieredSales, ERC2771ContextInternal {
     function _msgSender() internal view virtual override(Context, ERC2771ContextInternal) returns (address) {
