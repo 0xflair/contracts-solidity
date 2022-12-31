f49af46fec5caad55199c32f2daf9fa30c96aa6b
aramalipoor <aram.alipoor@gmail.com>
2022-12-31 15:10:57 +0100

    chore: deploy on all chains

--- deploy/02_deploy_finance_sales_facets.ts
+++ deploy/02_deploy_finance_sales_facets.ts
@@ -1,7 +1,7 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
 
-import {deployPermanentContract} from '../hardhat.util';
+import { deployPermanentContract } from '../hardhat.util';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const accounts = await hre.getUnnamedAccounts();
--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -4,5 +4,5 @@ cat package.json | \
     grep 'deploy:' | grep -v ':all"' | grep -v ':mainnet"' | grep -v ':arbitrumOne"' | \
     grep -oE '"([^"]+)":' | cut -d\" -f2 | \
     awk '{ print "npm run "$0; }' | \
-    bash -c 'while read -r line; do echo "$line"; $line || true; done'
+    bash -c 'while read -r line; do echo "$line"; $line; done'
     # || true
